d25ec4b387e2a52ef20e93cb0a68df6c68ca84f0
Nealle <74481574+JeknJok@users.noreply.github.com>
2025-02-14 12:30:38 -0800

    efedf
    sdfdsf

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,19 @@
 // pages/profile.tsx
+"use client"
 import React from "react";
 import BottomNav from "../components/NavBar";
+import { useQRCode } from "next-qrcode";
 
 const Profile = () => {
+ 
   // Simulated user data, add more or take from api
   const user = {
     name: "John Doe",
     email: "johndoe@example.com",
     joined: "2024-01-15",
   };
+
+  const { Canvas } = useQRCode();
 
   return (
     <main className="p-6 flex flex-col items-center text-center min-h-screen pb-16">
@@ -17,6 +22,23 @@ const Profile = () => {
         <p className="text-lg"><strong>Name:</strong> {user.name}</p>
         <p className="text-lg"><strong>Email:</strong> {user.email}</p>
         <p className="text-lg"><strong>Joined:</strong> {user.joined}</p>
+        <div className="p-6 flex flex-col items-center text-center min-h-screen pb-16x"> 
+
+          <p className="text-sm mt-2">Scan to share profile</p>
+          <Canvas
+          text={JSON.stringify(user)}
+          options={{
+            errorCorrectionLevel: 'M',
+            margin: 3,
+            scale: 4,
+            width: 200,
+            color: {
+              dark: '#010599FF',
+              light: '#FFBF60FF',
+            },
+          }}
+        />
+        </div>
       </div>
       <BottomNav />
     </main>
