c03825192962e569c9da843a7183e191193c2ec3
Nealle <74481574+JeknJok@users.noreply.github.com>
2025-02-14 11:25:21 -0800

    ok

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div><button className='button btn'onClick={()=> console.log('CLICK')}>ADD TO CART</button></div>
+  )
+}
+
+export default AddToCart
---
+++ app/users/page.tsx
@@ -0,0 +1,33 @@
+import { Nosifer } from 'next/font/google';
+import React from 'react'
+
+interface User{
+    id: number;
+    name: string;
+    email: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+        {next: {revalidate:10}});
+    const users : User[] = await res.json();
+    return (
+    <>
+    <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
+    <table className='table table-brodered'>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+            </tr>
+        </thead>
+        <tbody>{users.map(user=><tr key={user.id}><th>{user.name}</th>
+        <th>{user.email}</th></tr>)}</tbody>
+    </table>
+    </>
+  )
+}
+
+export default UsersPage
